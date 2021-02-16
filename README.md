c12cdb4dcc152aecad04f16d4cceb646925c75c5
Eran <ecohanim@gmail.com>
2021-02-16 13:39:15 +0200

    Url Params Query Params and Json Responses

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# echo-server-ex
+# Web Server Example with Echo High performance, extensible, minimalist Go web framework
 https://www.youtube.com/playlist?list=PLFmONUGpIk0YwlJMZOo21a9Q1juVrk4YY
--- web-server/main.go
+++ web-server/main.go
@@ -10,11 +10,32 @@ import (
 func yallo(c echo.Context) error {
 	return c.String(http.StatusOK, "yallo from the web side!")
 }
+
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\nand cat type is: %s\n", catName, catType))
+	} else if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "please specify data type (string|json)",
+	})
+}
+
 func main() {
 	fmt.Println("\nWelcome to CHQ Web Server")
 
 	e := echo.New()
 	e.GET("/", yallo)
+	e.GET("/cats/:data", getCats)
 
 	e.Start(":9090")
 }
